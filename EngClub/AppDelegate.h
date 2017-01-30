7cb5f0a46c1ce57f72a2f53f869bf4508c476c26
Anton Kriat <kifreez@gmail.com>
2017-01-30 13:02:05 +0200

    Initial Commit

---
+++ EngClub/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EngClub
+//
+//  Created by Kif on 30.01.17.
+//  Copyright © 2017 KifApp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EngClub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EngClub
+//
+//  Created by Kif on 30.01.17.
+//  Copyright © 2017 KifApp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
